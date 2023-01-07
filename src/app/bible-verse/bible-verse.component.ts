37bd592ab484195e6d181afe85b5c27c8a62b3b9
forjbj <forjbj@gmail.com>
2023-01-07 15:11:40 +1000

    issues

--- src/app/bible-verse/bible-verse.component.ts
+++ src/app/bible-verse/bible-verse.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import * as bibleJson from '../../assets/bible/Bible.json';
 import * as wasm from '../../../pkg';
 import { Meta, Title } from '@angular/platform-browser';
@@ -16,54 +16,58 @@ export class BibleVerseComponent {
   public observer?: IntersectionObserver;
   public bookSelected?: number;
   public testament?: number;
-  public bookName: any;
+  public bookName?: any;
 
   public bible: any = bibleJson;
 
   
-constructor( public title: Title,
+  constructor( public title: Title,
              public meta: Meta,
              public elementRef:ElementRef,) {
 
-  if (typeof Worker !== 'undefined') {
-    // Create a new
-    const worker = new Worker(new URL('./bible-verse.worker', import.meta.url));
-    worker.onmessage = ({ data }) => {
-      this.result = data;
-    };
-    worker.postMessage(wasm.render_widget());
-  } else {
-    // Web workers are not supported in this environment.
-    // You should add a fallback so that your program still executes correctly.
-    this.result = wasm.render_widget();
+  this.load();
   }
 
-}
-ngOnInit(){ }
+  load() {
+    this.threadWASM();
+    this.bibleInfo(); //wait till threadWASM is finished to run to get book info
+  }
 
-ngAfterViewInit(): void {
-  setTimeout(() => {
-    const name = this.elementRef.nativeElement.querySelector(".head");
-    const splits = name.id.toString().split('-');
-    this.testament = Number(splits[0]);
-    this.bookSelected = Number(splits[1]);
-    this.chapter = Number(splits[2]) + 1; // add 1 to get right chapter number
-    this.bookName = this.bible[this.testament].books[this.bookSelected].bookName;
-    const ver = name.getElementsByTagName("DIV");
-    this.verse = Math.floor(Math.random() * ver.length) + 1;
+  threadWASM() {
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker(new URL('./bible-verse.worker', import.meta.url));
+      worker.onmessage = ({ data }) => {
+        this.result = data;
+      };
+      worker.postMessage(wasm.render_widget());
+    } else {
+      // Web workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+      this.result = wasm.render_widget();
+    }
+  }
 
-    ver[this.verse -1].scrollIntoView({
-                          behavior: 'auto',
-                          block: 'center',
-                          inline: 'center'
-                      });
-  }, 100); // give it a moment to redraw
+  bibleInfo() {
+    if (this.elementRef?.nativeElement.querySelector(".head")) { //necessary or error for null values below on inital load
+      const name: any = this.elementRef?.nativeElement.querySelector(".head");
+      const splits = name.id.toString().split('-');
+      this.testament = Number(splits[0]);
+      this.bookSelected = Number(splits[1]);
+      this.chapter = Number(splits[2]) + 1; // add 1 to get right chapter number
+      this.bookName = this.bible[this.testament].books[this.bookSelected].bookName;
+      const ver = name?.getElementsByTagName("DIV");
+      this.verse = Math.floor(Math.random() * ver.length) + 1;
 
+      ver[this.verse -1].scrollIntoView({
+                            behavior: 'auto',
+                            block: 'center',
+                            inline: 'center'
+                        });
+    }
+  }
 }
-reload() {
- document.location.reload();
-}
-}
+
 export function read_file() {
 return JSON.stringify(bibleJson); // WASM WORKS! don't touch
 }
