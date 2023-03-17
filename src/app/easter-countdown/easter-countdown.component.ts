de7e3a0f7c37146778fb925d4cb4eecd785cde4d
forjbj <forjbj@gmail.com>
2023-03-17 13:54:45 +1000

    randomise clouds on countdown page

--- src/app/easter-countdown/easter-countdown.component.ts
+++ src/app/easter-countdown/easter-countdown.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { delay } from 'rxjs';
 import { TimeService } from '../time.service';
 
 @Component({
@@ -10,18 +11,35 @@ export class EasterCountdownComponent {
 
   private sunX: string;
   private sunY: string;
-  private el?: any;
+  private elSun?: any;
+  private cloudOneDelay: string;
+  private cloudTwoDelay: string;
+  private cloudThreeDelay: string;
+  private elCloudOne?: any;
+  private elCloudTwo?: any;
+  private elCloudThree?: any;
 
   constructor(public time: TimeService) {
 
     this.sunX = (Math.floor(Math.random() * (90 - 7 + 1) ) + 7).toString() + '%'; // 90% of width for sun to be in; not including first and last 5%
     this.sunY = (Math.floor(Math.random() * (40 - 7 + 1) ) + 7).toString() + '%'; // top 40% of height for sun to be in; not including top 7%
+    this.cloudOneDelay = (Math.floor(Math.random() * (-40 - -10) ) -7 ).toString() + 's'; // random animation delay for clouds; different start points
+    this.cloudTwoDelay = (Math.floor(Math.random() * (-40 - -10) ) -7 ).toString() + 's'; // random animation delay for clouds; different start points
+    this.cloudThreeDelay = (Math.floor(Math.random() * (-40 - -10) ) -7 ).toString() + 's'; // random animation delay for clouds; different start points
   }
   ngAfterViewInit() {
 
-    this.el = document.getElementById('sun');
-    this.el.style.top = this.sunY;
-    this.el.style.left = this.sunX;
+    this.elSun = document.getElementById('sun');
+    this.elSun.style.top = this.sunY;
+    this.elSun.style.left = this.sunX;
+
+    this.elCloudOne = document.getElementById('cloudOne')
+    this.elCloudTwo = document.getElementById('cloudTwo')
+    this.elCloudThree = document.getElementById('cloudThree')
+    this.elCloudOne.style.animationDelay = this.cloudOneDelay;
+    this.elCloudTwo.style.animationDelay = this.cloudTwoDelay;
+    this.elCloudThree.style.animationDelay = this.cloudThreeDelay;
+    console.log(this.cloudThreeDelay)
     // console.log(this.sunX, this.sunY)
 
 
